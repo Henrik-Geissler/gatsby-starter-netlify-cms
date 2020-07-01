79f3fb8252d2644e81a7a8da976c473695f2149f
Your Name <you@example.com>
2020-07-01 15:48:40 +0200

    a

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -24,14 +24,6 @@ const AboutPage = ({ data }) => {
 
   return (
     <Layout>
-      <Helmet>
-        <link 
-        rel="stylesheet" 
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-        crossorigin="anonymous"
-      ></link>
-      </Helmet>
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
