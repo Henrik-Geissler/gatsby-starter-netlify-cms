e66550220cf640d65eb83dab37fd86f7f50faf2f
Your Name <you@example.com>
2020-07-08 09:31:47 +0200

    ci(format): prettier

--- src/pages/__tests__/ExternalApi.test.js
+++ src/pages/__tests__/ExternalApi.test.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { ExternalApiComponent } from "../ExternalApi";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react'
+import { ExternalApiComponent } from '../ExternalApi'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
-jest.mock("@auth0/auth0-react", () => ({
+jest.mock('@auth0/auth0-react', () => ({
   useAuth0: jest.fn(() => ({
     isLoading: false,
     isAuthenticated: true,
-    getAccessTokenSilently: jest.fn(() => Promise.resolve("access-token")),
+    getAccessTokenSilently: jest.fn(() => Promise.resolve('access-token')),
   })),
   withAuthenticationRequired: jest.fn(),
-}));
+}))
 
-describe("The ExternalApi component", () => {
+describe('The ExternalApi component', () => {
   beforeEach(() => {
-    fetch.resetMocks();
-  });
+    fetch.resetMocks()
+  })
 
-  it("renders", () => {
-    render(<ExternalApiComponent />);
-  });
+  it('renders', () => {
+    render(<ExternalApiComponent />)
+  })
 
-  it("makes a call to the API when the button is clicked", async () => {
-    fetch.mockResponseOnce(JSON.stringify({ msg: "This is the API result" }));
+  it('makes a call to the API when the button is clicked', async () => {
+    fetch.mockResponseOnce(JSON.stringify({ msg: 'This is the API result' }))
 
-    render(<ExternalApiComponent />);
-    fireEvent.click(screen.getByText("Ping API"));
+    render(<ExternalApiComponent />)
+    fireEvent.click(screen.getByText('Ping API'))
 
-    await waitFor(() => screen.getByTestId("api-result"));
+    await waitFor(() => screen.getByTestId('api-result'))
 
     expect(
       await screen.findByText(/This is the API result/)
-    ).toBeInTheDocument();
-  });
-});
+    ).toBeInTheDocument()
+  })
+})
